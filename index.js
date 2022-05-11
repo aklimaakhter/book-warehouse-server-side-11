881e1c2511ca8b01d9a0831003df3ac64efc1fb9
aklimaakhter <aklimawebd@gmail.com>
2022-05-11 12:50:04 +0600

    datbase connected

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
-const port=process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -12,20 +12,33 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:<password>@cluster0.u5yan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5yan.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('book warehouse db connected')
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection =client.db('bookWarehouse').collection('inventory');
+        
+        app.get('/inventory', async(req,res)=>{
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const inventories = await cursor.toArray();
+            res.send(inventories);
+        })
+    }
+    finally {
 
-app.get('/',(req,res)=>{
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Book warehouse server')
 })
 
-app.listen(port,()=>{
-    console.log('Listening to port',port)
+app.listen(port, () => {
+    console.log('Listening to port', port)
 })
