742e77b4cae61030e8beee307a926498c99cab21
aklimaakhter <aklimawebd@gmail.com>
2022-05-11 13:31:34 +0600

    single inventory API create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -26,6 +26,13 @@ async function run() {
             const cursor = inventoryCollection.find(query);
             const inventories = await cursor.toArray();
             res.send(inventories);
+        })
+
+        app.get('/inventory/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const inventory = await inventoryCollection.findOne(query);
+            res.send(inventory)
         })
     }
     finally {
